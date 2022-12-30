a78d589193738fcffda359b3bd7eb76d786329f3
Maulik Kamdar <maulik@maulik-kamdar.com>
2022-12-29 20:44:34 -0500

    populating from alternate labels

--- umls_knowledge_graph/umls_graph_creator.py
+++ umls_knowledge_graph/umls_graph_creator.py
@@ -9,17 +9,22 @@ class UMLSGraphCreator():
 
     def populate_labels(self):
         umls_terms = pd.read_csv(self.data_folder + 'selected_vocab_terms.tsv', sep='\t')
+        alternate_labels = pd.read_csv(self.data_folder + 'alternate_labels.tsv', sep='\t')
         for k in umls_terms.values.tolist():
-        if not umls_G.has_node(k[0]):
-            self.umls_G.add_node(k[0], vocabcodes = set([]), preflabels = set([]), altlabels = set([]))
-        self.umls_G.nodes[k[0]]['vocabcodes'].add(k[3] + ": " + k[4])
-        if k[1] == 'P':
-            self.umls_G.nodes[k[0]]['preflabels'].add(str(k[5]).lower())
-        if k[1] == 'S':
-            self.umls_G.nodes[k[0]]['altlabels'].add(str(k[5]).lower())
-        if not k[2] in umls_reverse_lookup:
-            self.umls_reverse_lookup[k[2]] = []
-        self.umls_reverse_lookup[k[2]].append(k[0])
+            if not umls_G.has_node(k[0]):
+                self.umls_G.add_node(k[0], vocabcodes = set([]), preflabels = set([]), altlabels = set([]))
+            self.umls_G.nodes[k[0]]['vocabcodes'].add(k[3] + ": " + k[4])
+            if k[1] == 'P':
+                self.umls_G.nodes[k[0]]['preflabels'].add(str(k[5]).lower())
+            if k[1] == 'S':
+                self.umls_G.nodes[k[0]]['altlabels'].add(str(k[5]).lower())
+            if not k[2] in umls_reverse_lookup:
+                self.umls_reverse_lookup[k[2]] = []
+            self.umls_reverse_lookup[k[2]].append(k[0])
+
+        for k in alternate_labels.values.tolist():
+            if umls_G.has_node(k[0]):
+                self.umls_G.nodes[k[0]]['altlabels'].add(str(k[2]).lower())
 
     def populate_hierarchy_edges(self):
         umls_hierarchy = pd.read_csv(self.data_folder + 'hierarchies.tsv', sep="\t")
